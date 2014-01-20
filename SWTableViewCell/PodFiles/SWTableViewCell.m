fed9bac5239f0e782b17079d5ff30bb4208f5376
danielebogo <bogodaniele83@gmail.com>
2014-01-20 10:56:24 +0100

    Detect if right (or lef)t array is empty, to set the scroll in one direction

--- SWTableViewCell/PodFiles/SWTableViewCell.m
+++ SWTableViewCell/PodFiles/SWTableViewCell.m
@@ -521,7 +521,14 @@ static NSString * const kTableViewCellContentView = @"UITableViewCellContentView
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     self.tapGestureRecognizer.enabled = NO;
-    if (scrollView.contentOffset.x > [self leftUtilityButtonsWidth])
+    if ( scrollView.contentOffset.x < 0 && _leftUtilityButtons.count == 0 ) {
+        // Block scroll to right if _leftUtilityButtons.count is 0
+        scrollView.contentOffset = CGPointZero;
+    } else if ( scrollView.contentOffset.x > [self leftUtilityButtonsWidth] && _rightUtilityButtons.count == 0 ) {
+        // Block scroll to left if _rightUtilityButtons.count is 0
+        scrollView.contentOffset = [self scrollViewContentOffset];
+    }
+    else if (scrollView.contentOffset.x > [self leftUtilityButtonsWidth])
     {
         if ([self rightUtilityButtonsWidth] > 0)
         {
